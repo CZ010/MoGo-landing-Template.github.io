f96c224dd5741adfa1f596a243eb470d62f1b4b6
cz <starj1918@gmail.com>
2021-07-01 11:48:19 +0600

    add scripts

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+'use strict'
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 900,
+    });
+    $('.work-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 900,
+    });
+});
